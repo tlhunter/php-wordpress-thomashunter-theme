ed8ac1b261e1edf4c6d516ef665f168bbf03b019
Thomas Hunter <tlhunter@gmail.com>
2012-12-15 16:21:57 -0500

    Making homepage look okay

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ if (have_posts()) {
     while (have_posts()) {
         the_post();
 ?>
-        <header>
-            <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-            <time datetime="<?php the_time('Y-m-j') ?>"><?php the_time(get_option('date_format')) ?></time>
-        </header>
+        <div id="post-listing">
+            <section>
+                <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+                <time datetime="<?php the_time('Y-m-j') ?>"><?php the_time(get_option('date_format')) ?></time>
+            </section>
+        </div>
 <?php
     }
 }
