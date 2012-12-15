e5f59b0ba703029212b2b2b1ed8d5e2fd91fb084
Thomas Hunter <tlhunter@gmail.com>
2012-12-15 15:50:19 -0500

    Tweakz

--- comments.php
+++ comments.php
@@ -21,23 +21,23 @@ The comments page for Bones
 <?php if ( have_comments() ) : ?>
 	<h3 id="comments" class="h2"><?php comments_number(__('<span>No</span> Responses', 'bonestheme'), __('<span>One</span> Response', 'bonestheme'), _n('<span>%</span> Response', '<span>%</span> Responses', get_comments_number(),'bonestheme') );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
 
-	<nav id="comment-nav">
+	<div id="comment-nav">
 		<ul class="clearfix">
 	  		<li><?php previous_comments_link() ?></li>
 	  		<li><?php next_comments_link() ?></li>
 	 	</ul>
-	</nav>
+	</div>
 	
 	<ol class="commentlist">
 		<?php wp_list_comments('type=comment&callback=bones_comments'); ?>
 	</ol>
 	
-	<nav id="comment-nav">
+	<div id="comment-nav">
 		<ul class="clearfix">
 	  		<li><?php previous_comments_link() ?></li>
 	  		<li><?php next_comments_link() ?></li>
 		</ul>
-	</nav>
+	</div>
   
 	<?php else : // this is displayed if there are no comments so far ?>
 
