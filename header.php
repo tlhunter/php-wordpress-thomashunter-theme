a4ccfaf4ee947ed0d7b95f947ba8e61878c5e4e4
Thomas Hunter <tlhunter@gmail.com>
2012-12-16 00:38:53 -0500

    Tweaking title logic, renaming posts page

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php wp_title(''); ?></title>
+    <title><?php $subtitle = wp_title('', false); $title = get_bloginfo('name'); if ($subtitle) { echo $subtitle; } else { echo $title; } ?></title>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/library/css/reset.css" />
     <!--<link rel="stylesheet" type="text/css" href="library/css/style.css" />-->
@@ -33,7 +33,7 @@ if (isset($current_page_override)) {
 }
 ?>
             <li class="<?php if ($slug == 'home') { echo " active"; } ?>" id="dock-home"><a href="<?php echo home_url(); ?>" title="Homepage"><img alt="<?php bloginfo('name'); ?>" src="http://www.gravatar.com/avatar/e79802a826a113cdb9c49f5d04dc6946.png?s=200" /></a></li>
-            <li class="with-label<?php if ($slug == 'category-index' || $slug == 'post') { echo " active"; } ?>" id="dock-posts"><a title="Blogs Posts" href="<?=site_url();?>/category-index/"><div class="icon"></div><span>Posts</span></a></li>
+            <li class="with-label<?php if ($slug == 'posts' || $slug == 'post') { echo " active"; } ?>" id="dock-posts"><a title="Blogs Posts" href="<?=site_url();?>/posts/"><div class="icon"></div><span>Posts</span></a></li>
             <li class="with-label<?php if ($slug == 'projects') { echo " active"; } ?>" id="dock-projects"><a title="Projects by Thomas Hunter" href="<?=site_url();?>/projects"><div class="icon"></div><span>Projects</span></a></li>
             <li class="with-label<?php if ($slug == 'about') { echo " active"; } ?>" id="dock-about"><a href="<?=site_url();?>/about" title="About Thomas Hunter"><div class="icon"></div><span>About</span></a></li>
             <li class="with-label<?php if ($slug == 'search') { echo " active"; } ?>" id="dock-search"><a title="Search over 300 posts" href="#"><div class="icon"></div><span>Search</span></a></li>
