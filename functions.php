6527cac5e1ed12bd4f0b992272d462347b12c7b8
Thomas Hunter <tlhunter@gmail.com>
2012-12-16 18:08:42 -0500

    comments look decent

--- functions.php
+++ functions.php
@@ -131,9 +131,11 @@ function bones_comments($comment, $args, $depth) {
                 ?>
                 <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=64" class="load-gravatar avatar avatar-48 photo" height="64" width="64" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
                 <!-- end custom gravatar call -->
-                <?php printf(__('<cite class="fn">%s</cite>', 'bonestheme'), get_comment_author_link()) ?>
-                <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__('F jS, Y', 'bonestheme')); ?> </a></time>
-                <?php edit_comment_link(__('(Edit)', 'bonestheme'),'  ','') ?>
+                <div class="comment-info">
+                    <?php printf(__('<cite class="fn">%s</cite>', 'bonestheme'), get_comment_author_link()) ?>
+                    <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__('F jS, Y', 'bonestheme')); ?> </a></time>
+                </div>
+                <div style="clear: both;"></div>
             </header>
             <?php if ($comment->comment_approved == '0') : ?>
                 <div class="alert info">
@@ -144,6 +146,7 @@ function bones_comments($comment, $args, $depth) {
                 <?php comment_text() ?>
             </section>
             <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+            <?php edit_comment_link(__('(Edit)', 'bonestheme'),'  ','') ?>
         </div>
     <!-- </li> is added by WordPress automatically -->
 <?php
