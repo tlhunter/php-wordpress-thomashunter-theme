1fdf8b57549f75c17ab7133a57d85445686b3dd3
Thomas Hunter <tlhunter@gmail.com>
2012-12-20 22:10:04 -0500

    Edit Link on Posts #3

--- single.php
+++ single.php
@@ -8,6 +8,7 @@ if (have_posts()) {
         <header>
             <h1><?php the_title(); ?></h1>
             <time datetime="<?php the_time('Y-m-j') ?>"><?php the_time(get_option('date_format')) ?></time>
+            <?php edit_post_link( __( 'Edit'), '<p>', '</p>' ); ?>
         </header>
 <?php
 if ( has_post_thumbnail() ) {
